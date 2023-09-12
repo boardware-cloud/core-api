f31167cd9387ecf248aee98f746c6aae0ad4719f
Dan Chen <dan.chen@boardware.com>
2023-09-12 14:00:04 +0800

    feat

--- coreapi.go
+++ coreapi.go
@@ -6,57 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type PubKeyCredParam struct {
-	Type string `json:"type"`
-	Alg  int64  `json:"alg"`
-}
-type Error struct {
-	StatusCode int64  `json:"statusCode"`
-	Code       int64  `json:"code"`
-	Message    string `json:"message"`
-}
-type CreateTicketRequest struct {
-	Email            string     `json:"email"`
-	Type             TicketType `json:"type"`
-	Password         *string    `json:"password,omitempty"`
-	TotpCode         *string    `json:"totpCode,omitempty"`
-	VerificationCode *string    `json:"verificationCode,omitempty"`
-}
-type Account struct {
-	Id      string `json:"id"`
-	Email   string `json:"email"`
-	Role    Role   `json:"role"`
-	HasTotp bool   `json:"hasTotp"`
-}
-type Pagination struct {
-	Index int64 `json:"index"`
-	Limit int64 `json:"limit"`
-	Total int64 `json:"total"`
-}
-type WebAuthnResponse struct {
-	AttestationObject string `json:"attestationObject"`
-	ClientDataJSON    string `json:"clientDataJSON"`
-}
-type WebauthnUser struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	DisplayName string `json:"displayName"`
-}
-type Ticket struct {
-	Token string     `json:"token"`
-	Type  TicketType `json:"type"`
-}
-type PutTotpRequest struct {
-	Url      string   `json:"url"`
-	TotpCode string   `json:"totpCode"`
-	Tickets  []string `json:"tickets"`
+type SessionVerification struct {
+	Status SessionStatus `json:"status"`
 }
 type CreateTicketChallenge struct {
 	Email string `json:"email"`
-}
-type AuthenticatorSelection struct {
-	RequireResidentKey bool   `json:"requireResidentKey"`
-	UserVerification   string `json:"userVerification"`
 }
 type CreateSessionRequest struct {
 	Email            *string   `json:"email,omitempty"`
@@ -65,93 +19,131 @@ type CreateSessionRequest struct {
 	VerificationCode *string   `json:"verificationCode,omitempty"`
 	Tickets          *[]string `json:"tickets,omitempty"`
 }
+type CreateTicketRequest struct {
+	Email            string     `json:"email"`
+	Type             TicketType `json:"type"`
+	Password         *string    `json:"password,omitempty"`
+	TotpCode         *string    `json:"totpCode,omitempty"`
+	VerificationCode *string    `json:"verificationCode,omitempty"`
+}
+type Authentication struct {
+	Factors []string `json:"factors"`
+}
+type PubKeyCredParam struct {
+	Type string `json:"type"`
+	Alg  int64  `json:"alg"`
+}
+type CreateVerificationCodeRequest struct {
+	Email   *string                 `json:"email,omitempty"`
+	Purpose VerificationCodePurpose `json:"purpose"`
+}
+type Ticket struct {
+	Token string     `json:"token"`
+	Type  TicketType `json:"type"`
+}
+type Totp struct {
+	Url string `json:"url"`
+}
+type Pagination struct {
+	Index int64 `json:"index"`
+	Limit int64 `json:"limit"`
+	Total int64 `json:"total"`
+}
+type WebAuthn struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt int64  `json:"createdAt"`
+	Os        string `json:"os"`
+}
+type WebAuthnResponse struct {
+	AttestationObject string `json:"attestationObject"`
+	ClientDataJSON    string `json:"clientDataJSON"`
+}
 type CredentialCreationResponse struct {
-	RawId      string           `json:"rawId"`
-	Response   WebAuthnResponse `json:"response"`
-	Transports []string         `json:"transports"`
 	Name       string           `json:"name"`
 	Os         string           `json:"os"`
 	Type       string           `json:"type"`
 	Id         string           `json:"id"`
+	RawId      string           `json:"rawId"`
+	Response   WebAuthnResponse `json:"response"`
+	Transports []string         `json:"transports"`
+}
+type Rp struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 type AccountList struct {
 	Data       []Account  `json:"data"`
 	Pagination Pagination `json:"pagination"`
+}
+type PutTotpRequest struct {
+	Url      string   `json:"url"`
+	TotpCode string   `json:"totpCode"`
+	Tickets  []string `json:"tickets"`
+}
+type PublicKey struct {
+	Timeout                int64                  `json:"timeout"`
+	AuthenticatorSelection AuthenticatorSelection `json:"authenticatorSelection"`
+	Rp                     Rp                     `json:"rp"`
+	User                   WebauthnUser           `json:"user"`
+	Challenge              string                 `json:"challenge"`
+	PubKeyCredParams       []PubKeyCredParam      `json:"pubKeyCredParams"`
+}
+type Error struct {
+	StatusCode int64  `json:"statusCode"`
+	Code       int64  `json:"code"`
+	Message    string `json:"message"`
+}
+type Account struct {
+	Id      string `json:"id"`
+	Email   string `json:"email"`
+	Role    Role   `json:"role"`
+	HasTotp bool   `json:"hasTotp"`
+}
+type CreateAccountRequest struct {
+	VerificationCode *string `json:"verificationCode,omitempty"`
+	Email            string  `json:"email"`
+	Password         string  `json:"password"`
+	Role             *Role   `json:"role,omitempty"`
 }
 type Token struct {
 	Secret      string `json:"secret"`
 	TokenType   string `json:"tokenType"`
 	TokenFormat string `json:"tokenFormat"`
 }
-type Session struct {
-	ExpiredAt   int64         `json:"expiredAt"`
-	CreatedAt   int64         `json:"createdAt"`
-	Status      SessionStatus `json:"status"`
-	Account     Account       `json:"account"`
-	Token       string        `json:"token"`
-	TokenType   string        `json:"tokenType"`
-	TokenFormat string        `json:"tokenFormat"`
+type WebAuthnSession struct {
+	Id        string    `json:"id"`
+	PublicKey PublicKey `json:"publicKey"`
 }
-type CreateAccountRequest struct {
-	Email            string  `json:"email"`
-	Password         string  `json:"password"`
-	Role             *Role   `json:"role,omitempty"`
-	VerificationCode *string `json:"verificationCode,omitempty"`
+type AuthenticatorSelection struct {
+	RequireResidentKey bool   `json:"requireResidentKey"`
+	UserVerification   string `json:"userVerification"`
 }
-type Authentication struct {
-	Factors []string `json:"factors"`
+type WebauthnUser struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName"`
 }
 type SessionVerificationRequest struct {
 	Token string `json:"token"`
+}
+type UpdatePasswordRequest struct {
+	Email            string  `json:"email"`
+	VerificationCode *string `json:"verificationCode,omitempty"`
+	Password         *string `json:"password,omitempty"`
+	NewPassword      string  `json:"newPassword"`
 }
 type CreateVerificationCodeRespones struct {
 	Email   *string                 `json:"email,omitempty"`
 	Purpose VerificationCodePurpose `json:"purpose"`
 	Result  VerificationCodeResult  `json:"result"`
 }
-type CreateVerificationCodeRequest struct {
-	Email   *string                 `json:"email,omitempty"`
-	Purpose VerificationCodePurpose `json:"purpose"`
+type Session struct {
+	Id        string        `json:"id"`
+	Status    SessionStatus `json:"status"`
+	ExpiredAt int64         `json:"expiredAt"`
+	CreatedAt int64         `json:"createdAt"`
 }
-type UpdatePasswordRequest struct {
-	Password         *string `json:"password,omitempty"`
-	NewPassword      string  `json:"newPassword"`
-	Email            string  `json:"email"`
-	VerificationCode *string `json:"verificationCode,omitempty"`
-}
-type Totp struct {
-	Url string `json:"url"`
-}
-type SessionVerification struct {
-	Status SessionStatus `json:"status"`
-}
-type WebAuthn struct {
-	Os        string `json:"os"`
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	CreatedAt int64  `json:"createdAt"`
-}
-type PublicKey struct {
-	Challenge              string                 `json:"challenge"`
-	PubKeyCredParams       []PubKeyCredParam      `json:"pubKeyCredParams"`
-	Timeout                int64                  `json:"timeout"`
-	AuthenticatorSelection AuthenticatorSelection `json:"authenticatorSelection"`
-	Rp                     Rp                     `json:"rp"`
-	User                   WebauthnUser           `json:"user"`
-}
-type WebAuthnSession struct {
-	Id        string    `json:"id"`
-	PublicKey PublicKey `json:"publicKey"`
-}
-type Rp struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-type Ordering string
-
-const ASCENDING Ordering = "ASCENDING"
-const DESCENDING Ordering = "DESCENDING"
-
 type VerificationCodePurpose string
 
 const CREATE_ACCOUNT VerificationCodePurpose = "CREATE_ACCOUNT"
@@ -159,6 +151,17 @@ const SET_PASSWORD VerificationCodePurpose = "SET_PASSWORD"
 const SIGNIN VerificationCodePurpose = "SIGNIN"
 const CREATE_2FA VerificationCodePurpose = "CREATE_2FA"
 const TICKET VerificationCodePurpose = "TICKET"
+
+type SessionStatus string
+
+const ACTIVED SessionStatus = "ACTIVED"
+const EXPIRED SessionStatus = "EXPIRED"
+const DISACTIVED SessionStatus = "DISACTIVED"
+
+type Ordering string
+
+const ASCENDING Ordering = "ASCENDING"
+const DESCENDING Ordering = "DESCENDING"
 
 type VerificationCodeResult string
 
@@ -180,58 +183,49 @@ const ADMIN Role = "ADMIN"
 const USER Role = "USER"
 const SUB_ACCOUNT Role = "SUB_ACCOUNT"
 
-type SessionStatus string
-
-const ACTIVED SessionStatus = "ACTIVED"
-const EXPIRED SessionStatus = "EXPIRED"
-const DISACTIVED SessionStatus = "DISACTIVED"
-
 type AccountApiInterface interface {
-	ListWebAuthn(gin_context *gin.Context)
-	CreateWebauthnTickets(gin_context *gin.Context, id string)
-	CreateAccount(gin_context *gin.Context, gin_body CreateAccountRequest)
-	ListAccount(gin_context *gin.Context, ordering Ordering, index int64, limit int64)
-	VerifySession(gin_context *gin.Context, gin_body SessionVerificationRequest)
-	GetAuthentication(gin_context *gin.Context, email string)
-	CreateWebAuthnChallenge(gin_context *gin.Context)
 	CreateWebauthnTicketChallenge(gin_context *gin.Context, gin_body CreateTicketChallenge)
 	CreateWebauthn(gin_context *gin.Context, id string)
+	UpdatePassword(gin_context *gin.Context, gin_body UpdatePasswordRequest)
+	VerifySession(gin_context *gin.Context, gin_body SessionVerificationRequest)
+	GetAuthentication(gin_context *gin.Context, email string)
+	ListWebAuthn(gin_context *gin.Context)
 	DeleteWebAuthn(gin_context *gin.Context, id string)
+	GetAccount(gin_context *gin.Context)
+	CreateWebAuthnChallenge(gin_context *gin.Context)
+	CreateWebauthnTickets(gin_context *gin.Context, id string)
+	DeleteTotp(gin_context *gin.Context)
 	GetTotp(gin_context *gin.Context)
 	CreateTotp2FA(gin_context *gin.Context, gin_body PutTotpRequest)
-	DeleteTotp(gin_context *gin.Context)
 	CreateSession(gin_context *gin.Context, gin_body CreateSessionRequest)
-	GetAccount(gin_context *gin.Context)
-	UpdatePassword(gin_context *gin.Context, gin_body UpdatePasswordRequest)
+	CreateAccount(gin_context *gin.Context, gin_body CreateAccountRequest)
+	ListAccount(gin_context *gin.Context, ordering Ordering, index int64, limit int64, roles []string)
 }
 
-func ListWebAuthnBuilder(api AccountApiInterface) func(c *gin.Context) {
+func CreateWebauthnTicketChallengeBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
-		api.ListWebAuthn(gin_context)
-	}
-}
-func CreateWebauthnTicketsBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.CreateWebauthnTickets(gin_context, id)
-	}
-}
-func CreateAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		var createAccountRequest CreateAccountRequest
-		if err := gin_context.ShouldBindJSON(&createAccountRequest); err != nil {
+		var createTicketChallenge CreateTicketChallenge
+		if err := gin_context.ShouldBindJSON(&createTicketChallenge); err != nil {
 			gin_context.JSON(400, gin.H{})
 			return
 		}
-		api.CreateAccount(gin_context, createAccountRequest)
+		api.CreateWebauthnTicketChallenge(gin_context, createTicketChallenge)
 	}
 }
-func ListAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
+func CreateWebauthnBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
-		ordering := gin_context.Query("ordering")
-		index := gin_context.Query("index")
-		limit := gin_context.Query("limit")
-		api.ListAccount(gin_context, Ordering(ordering), stringToInt64(index), stringToInt64(limit))
+		id := gin_context.Param("id")
+		api.CreateWebauthn(gin_context, id)
+	}
+}
+func UpdatePasswordBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		var updatePasswordRequest UpdatePasswordRequest
+		if err := gin_context.ShouldBindJSON(&updatePasswordRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdatePassword(gin_context, updatePasswordRequest)
 	}
 }
 func VerifySessionBuilder(api AccountApiInterface) func(c *gin.Context) {
@@ -250,31 +244,36 @@ func GetAuthenticationBuilder(api AccountApiInterface) func(c *gin.Context) {
 		api.GetAuthentication(gin_context, email)
 	}
 }
-func CreateWebAuthnChallengeBuilder(api AccountApiInterface) func(c *gin.Context) {
+func ListWebAuthnBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
-		api.CreateWebAuthnChallenge(gin_context)
-	}
-}
-func CreateWebauthnTicketChallengeBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		var createTicketChallenge CreateTicketChallenge
-		if err := gin_context.ShouldBindJSON(&createTicketChallenge); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.CreateWebauthnTicketChallenge(gin_context, createTicketChallenge)
-	}
-}
-func CreateWebauthnBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.CreateWebauthn(gin_context, id)
+		api.ListWebAuthn(gin_context)
 	}
 }
 func DeleteWebAuthnBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		id := gin_context.Param("id")
 		api.DeleteWebAuthn(gin_context, id)
+	}
+}
+func GetAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		api.GetAccount(gin_context)
+	}
+}
+func CreateWebAuthnChallengeBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		api.CreateWebAuthnChallenge(gin_context)
+	}
+}
+func CreateWebauthnTicketsBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.CreateWebauthnTickets(gin_context, id)
+	}
+}
+func DeleteTotpBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		api.DeleteTotp(gin_context)
 	}
 }
 func GetTotpBuilder(api AccountApiInterface) func(c *gin.Context) {
@@ -292,11 +291,6 @@ func CreateTotp2FABuilder(api AccountApiInterface) func(c *gin.Context) {
 		api.CreateTotp2FA(gin_context, putTotpRequest)
 	}
 }
-func DeleteTotpBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		api.DeleteTotp(gin_context)
-	}
-}
 func CreateSessionBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		var createSessionRequest CreateSessionRequest
@@ -307,38 +301,42 @@ func CreateSessionBuilder(api AccountApiInterface) func(c *gin.Context) {
 		api.CreateSession(gin_context, createSessionRequest)
 	}
 }
-func GetAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
+func CreateAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
-		api.GetAccount(gin_context)
-	}
-}
-func UpdatePasswordBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		var updatePasswordRequest UpdatePasswordRequest
-		if err := gin_context.ShouldBindJSON(&updatePasswordRequest); err != nil {
+		var createAccountRequest CreateAccountRequest
+		if err := gin_context.ShouldBindJSON(&createAccountRequest); err != nil {
 			gin_context.JSON(400, gin.H{})
 			return
 		}
-		api.UpdatePassword(gin_context, updatePasswordRequest)
+		api.CreateAccount(gin_context, createAccountRequest)
+	}
+}
+func ListAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		ordering := gin_context.Query("ordering")
+		index := gin_context.Query("index")
+		limit := gin_context.Query("limit")
+		roles := gin_context.QueryArray("roles")
+		api.ListAccount(gin_context, Ordering(ordering), stringToInt64(index), stringToInt64(limit), (roles))
 	}
 }
 func AccountApiInterfaceMounter(gin_router *gin.Engine, gwg_api_label AccountApiInterface) {
-	gin_router.GET("/account/webauthn", ListWebAuthnBuilder(gwg_api_label))
-	gin_router.POST("/account/webauthn/sessions/tickets/:id", CreateWebauthnTicketsBuilder(gwg_api_label))
-	gin_router.POST("/accounts", CreateAccountBuilder(gwg_api_label))
-	gin_router.GET("/accounts", ListAccountBuilder(gwg_api_label))
-	gin_router.GET("/accounts/session/verification", VerifySessionBuilder(gwg_api_label))
-	gin_router.GET("/account/authentication", GetAuthenticationBuilder(gwg_api_label))
-	gin_router.POST("/account/webauthn/sessions/challenge", CreateWebAuthnChallengeBuilder(gwg_api_label))
 	gin_router.POST("/account/webauthn/sessions/tickets/challenge", CreateWebauthnTicketChallengeBuilder(gwg_api_label))
 	gin_router.POST("/account/webauthn/sessions/:id", CreateWebauthnBuilder(gwg_api_label))
+	gin_router.PUT("/account/password", UpdatePasswordBuilder(gwg_api_label))
+	gin_router.GET("/accounts/session/verification", VerifySessionBuilder(gwg_api_label))
+	gin_router.GET("/account/authentication", GetAuthenticationBuilder(gwg_api_label))
+	gin_router.GET("/account/webauthn", ListWebAuthnBuilder(gwg_api_label))
 	gin_router.DELETE("/account/webauthn/:id", DeleteWebAuthnBuilder(gwg_api_label))
+	gin_router.GET("/account", GetAccountBuilder(gwg_api_label))
+	gin_router.POST("/account/webauthn/sessions/challenge", CreateWebAuthnChallengeBuilder(gwg_api_label))
+	gin_router.POST("/account/webauthn/sessions/tickets/:id", CreateWebauthnTicketsBuilder(gwg_api_label))
+	gin_router.DELETE("/account/totp", DeleteTotpBuilder(gwg_api_label))
 	gin_router.GET("/account/totp", GetTotpBuilder(gwg_api_label))
 	gin_router.PUT("/account/totp", CreateTotp2FABuilder(gwg_api_label))
-	gin_router.DELETE("/account/totp", DeleteTotpBuilder(gwg_api_label))
 	gin_router.POST("/account/session", CreateSessionBuilder(gwg_api_label))
-	gin_router.GET("/account", GetAccountBuilder(gwg_api_label))
-	gin_router.PUT("/account/password", UpdatePasswordBuilder(gwg_api_label))
+	gin_router.POST("/accounts", CreateAccountBuilder(gwg_api_label))
+	gin_router.GET("/accounts", ListAccountBuilder(gwg_api_label))
 }
 
 type ServicesApiInterface interface {
